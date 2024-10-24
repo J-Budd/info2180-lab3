5f2ce989b2eb2af5f524b25bc779c39e62206bbf
Jillian Budd <jillianabudd@gmail.com>
2024-10-24 11:34:47 -0500

    Click function working

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,5 +3,21 @@
 //initialises board
 document.addEventListener('DOMContentLoaded', () => {
     const square = document.querySelectorAll("#board div");
-    square.forEach(square => square.className = 'square');
-})
+    square.forEach(square => square.classList.add('square'));
+    square.forEach(square => square.addEventListener('click', squareClicked));
+    for(let count = 0; count < 9; count++){
+        document.querySelector('square')[count].setAttribute('square', count);
+    }
+});
+
+function squareClicked(e){
+    const starget = e.target;
+
+    if (starget.innerHTML != ""){
+        return;
+    }
+
+    starget.classList.add('square.X');
+    starget.innerHTML = "X";
+}
+
