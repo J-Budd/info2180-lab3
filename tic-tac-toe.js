58641cd2103dd6234e70fa97db2d06390ae27599
Jillian Budd <jillianabudd@gmail.com>
2024-10-25 10:01:51 -0500

    Hover function working

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,32 @@
 "use strict";
+let player = 'X';
+const winConditions = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]   
+]
 
-//initialises board
+//Ex 1
 document.addEventListener('DOMContentLoaded', () => {
     const square = document.querySelectorAll("#board div");
     square.forEach(square => square.classList.add('square'));
+    square.forEach(square => square.addEventListener('mouseover', (e) => {
+        e.target.classList.add('hover');
+    }));
+    square.forEach(square => square.addEventListener('mouseout', (e) => {
+        e.target.classList.remove('hover');
+    }));
     square.forEach(square => square.addEventListener('click', squareClicked));
 });
 
-let player = 'X';
-let played = [];
 
+
+//Ex 2
 function squareClicked(e){
     const starget = e.target;
 
@@ -21,10 +38,17 @@ function squareClicked(e){
         starget.classList.add("X");
         starget.innerHTML = "X";
         player = 'O';
-        alert("Index: ", starget);
     } else {
         starget.classList.add('O');
         starget.innerHTML = "O";
         player = 'X';
     }
+
+    checkWinner();
+}
+
+
+//Ex 4
+function checkWinner(){
+
 }
